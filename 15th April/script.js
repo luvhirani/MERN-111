1a40469eb14aac3a563dc63fbae605eae9cf7a9c
luvhirani <luvhirani0@gmail.com>
2025-04-15 14:47:22 +0530

    github commands

---
+++ 15th April/script.js
@@ -0,0 +1,14 @@
+// Git 
+// git init 
+// git add ./ git add filname
+// git commit -m "msg"
+// git remote add origin main 
+// git push origin main
+
+// Git Branch 
+// git branch feature / git checkout -b feature
+// git checkout feature
+// git add ./ git add filname
+// git commit -m "msg"
+// git remote add origin main 
+// git push
