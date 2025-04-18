402bbb7ccae16230457f6f36e67965768c332e8f
luvhirani <luvhirani0@gmail.com>
2025-04-18 14:55:58 +0530

    19th April

---
+++ 19th April/script.js
@@ -0,0 +1,24 @@
+//HOF
+// function hof (func){
+//     console.log("hof function")
+// }
+
+// function callback() {
+//     console.log("callback function")
+// }
+
+// hof(callback())
+
+// function greet(name, callback) {
+ 
+// }
+
+// greet("luv")
+
+{
+    //block scope
+    console.log("block this:",this)
+}
+
+console.log("globalthis",this)
+
